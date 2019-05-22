3ca424822ff9caed15a80a69d1ee8fa4eecd074b
seven <seven@sevendeMBP.lan>
2019-05-22 09:46:30 +0800

    led match

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
-module.exports = function main() {
+module.exports = function main(a) {
+    var one = new Array(new Array("._.", "...", "._.", "._.", "...", "._.", "._.", "._.", "._.", "._."),
+        new Array("|.|", "..|", "._|", "._|", "|_|", "|_.", "|_.", "..|", "|_|", "|_|"),
+        new Array("|_|", "..|", "|_.", "._|", "..|", "._|", "|_|", "..|", "|_|", "..|"));
+
+    var num = a.toString();
+    var led = "";
+
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < num.length - 1; j++) {
+            led += one[i][num[j]] + " ";
+        }
+        led += one[i][num[num.length - 1]] + "\n";
+    }
+
     console.log("Debug Info");
-    return 'Hello World!';
+    return led;
 };
